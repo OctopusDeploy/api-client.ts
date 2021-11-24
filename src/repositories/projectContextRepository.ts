47fe84ab0fbba7cd9f6a05c89789b6e0330d0b88
John Bristowe <john.bristowe@octopus.com>
2021-11-24 16:42:15 +1000

    hack(project contexts): removed linkage for deployment processes (temporary)

--- src/repositories/projectContextRepository.ts
+++ src/repositories/projectContextRepository.ts
@@ -7,12 +7,12 @@ import { VcsRunbookRepository } from "./vcsRunbookRepository";
 
 export class ProjectContextRepository {
     Branches: BranchesRepository;
-    DeploymentProcesses: DeploymentProcessRepository;
+    // DeploymentProcesses: DeploymentProcessRepository;
     DeploymentSettings: DeploymentSettingsRepository;
     Runbooks: VcsRunbookRepository;
 
     constructor(client: Client, project: ProjectResource, branch: VcsBranchResource | undefined) {
-        this.DeploymentProcesses = new DeploymentProcessRepository(client, project, branch);
+        // this.DeploymentProcesses = new DeploymentProcessRepository(client, project, branch);
         this.DeploymentSettings = new DeploymentSettingsRepository(client, project, branch);
         this.Runbooks = new VcsRunbookRepository(client, project, branch);
         this.Branches = new BranchesRepository(client);
