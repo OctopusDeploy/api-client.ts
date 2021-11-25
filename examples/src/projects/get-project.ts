418aa95672cbb2b01242300a4a2309c0edc4f62c
John Bristowe <john.bristowe@octopus.com>
2021-11-25 12:20:45 +1000

    feat(examples): added example for projects

---
+++ examples/src/projects/get-project.ts
@@ -0,0 +1,44 @@
+import { Client, ClientConfiguration, Repository } from '@octopusdeploy/api-client';
+import { ProjectResource } from '@octopusdeploy/message-contracts';
+
+const main = async () => {
+    const configuration: ClientConfiguration = {
+        apiKey: 'api-key', // required
+        apiUri: 'api-uri', // required
+        autoConnect: true
+    };
+
+    let client: Client | undefined;
+
+    try {
+        client = await Client.create(configuration);
+    } catch (error) {
+        console.error('The TypeScript API client could not be constructed.');
+        return;
+    }
+
+    if (client === null || client === undefined) {
+        return;
+    }
+
+    const repository = new Repository(client);
+    const projectNameOrId: string = 'project-name-or-ID';
+
+    console.log(`Getting project, "${projectNameOrId}"...`);
+
+    let project: ProjectResource | undefined;
+
+    try {
+        project = await repository.projects.find(projectNameOrId);
+    } catch (error) {
+        console.error(error);
+    }
+
+    if (project !== null && project !== undefined) {
+        console.log(`Project found: "${project?.Name}" (${project?.Id})`);
+    } else {
+        console.error(`Project, "${projectNameOrId}" not found`);
+    }
+}
+
+main();
