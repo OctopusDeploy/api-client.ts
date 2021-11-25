55b351248a4070ae025d92c3b3fc4799f74abd4a
John Bristowe <john.bristowe@octopus.com>
2021-11-25 12:20:32 +1000

    feat(examples): added examples for accounts

---
+++ examples/src/accounts/create-google-cloud-account.ts
@@ -0,0 +1,42 @@
+import { Client, ClientConfiguration, Repository } from '@octopusdeploy/api-client';
+import { NewGoogleCloudAccount, NewSensitiveValue } from '@octopusdeploy/message-contracts';
+
+const main = async () => {
+    const configuration: ClientConfiguration = {
+        apiKey: 'api-key', // required
+        apiUri: 'api-uri', // required
+        autoConnect: true
+    };
+
+    let client: Client | undefined;
+
+    try {
+        client = await Client.create(configuration);
+    } catch (error) {
+        console.error('The TypeScript API client could not be constructed.');
+        return;
+    }
+
+    if (client === null || client === undefined) {
+        return;
+    }
+
+    const repository = new Repository(client);
+
+    const jsonKey = NewSensitiveValue('fake-json-key');
+    const name = 'Google Cloud Account (OK to Delete)';
+
+    // define a new Google cloud account
+    const account = NewGoogleCloudAccount(name, jsonKey);
+
+    try {
+        // create account
+        await repository.accounts.create(account);
+    }
+    catch (error) {
+        console.error(error);
+        return;
+    }
+}
+
+main();
