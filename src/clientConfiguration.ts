a719fee8307dfc5148cb12558cf9954dc25e29cf
John Bristowe <john.bristowe@octopus.com>
2022-06-21 09:45:46 +1000

    chore: updated defaults

--- src/clientConfiguration.ts
+++ src/clientConfiguration.ts
@@ -12,22 +12,23 @@ export interface ClientConfiguration {
 }
 
 export function processConfiguration(configuration?: ClientConfiguration): ClientConfiguration {
-    const apiKey = process.env[EnvironmentVariables.ApiKey];
-    const host = process.env[EnvironmentVariables.Host];
-    const space = process.env[EnvironmentVariables.Space];
+    const apiKey = process.env[EnvironmentVariables.ApiKey] || "";
+    const host = process.env[EnvironmentVariables.Host] || "";
+    const space = process.env[EnvironmentVariables.Space] || "";
 
     if (!configuration) {
         return {
-            apiKey: apiKey || "",
-            apiUri: host || "",
+            apiKey: apiKey,
+            apiUri: host,
             autoConnect: true,
             space: space,
         };
     }
 
     return {
-        apiKey: !configuration.apiKey || configuration.apiKey.length === 0 ? apiKey || "" : configuration.apiKey,
-        apiUri: !configuration.apiUri || configuration.apiUri.length === 0 ? host || "" : configuration.apiUri,
-        space: !configuration.space || configuration.space.length === 0 ? space || "" : configuration.space,
+        apiKey: !configuration.apiKey || configuration.apiKey.length === 0 ? apiKey : configuration.apiKey,
+        apiUri: !configuration.apiUri || configuration.apiUri.length === 0 ? host : configuration.apiUri,
+        autoConnect: !configuration.autoConnect ? true : configuration.autoConnect,
+        space: !configuration.space || configuration.space.length === 0 ? space : configuration.space,
     };
 }
