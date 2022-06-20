44cc7b2748111f529605010303dd0e4beb23fb61
John Bristowe <john.bristowe@octopus.com>
2022-06-21 06:24:37 +1000

    feat: added environment variables constants

---
+++ src/environmentVariables.ts
@@ -0,0 +1,8 @@
+export const EnvironmentVariables = {
+    ApiKey: "OCTOPUS_API_KEY",
+    Host: "OCTOPUS_HOST",
+    Proxy: "OCTOPUS_PROXY",
+    ProxyPassword: "OCTOPUS_PROXY_PASSWORD",
+    ProxyUsername: "OCTOPUS_PROXY_USERNAME",
+    Space: "OCTOPUS_SPACE",
+} as const;
