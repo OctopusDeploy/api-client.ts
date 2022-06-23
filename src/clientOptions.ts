1ebcaf685260f61ffb2ae1c22e8f0bda4d8a69ee
John Bristowe <john.bristowe@octopus.com>
2022-06-23 12:58:59 +1000

    chore: applied Prettier formatting

--- src/clientOptions.ts
+++ src/clientOptions.ts
@@ -1,23 +1,23 @@
+import type { ClientConfiguration } from "./clientConfiguration";
 import type { ClientErrorResponseDetails } from "./clientErrorResponseDetails";
 import type { ClientRequestDetails } from "./clientRequestDetails";
 import type { ClientResponseDetails } from "./clientResponseDetails";
-import type { ClientConfiguration } from "./clientConfiguration";
 import type { ClientSession } from "./clientSession";
 import type { ServerInformation } from "./serverInformation";
 
 export interface ClientOptions {
-  configuration: ClientConfiguration;
-  session: ClientSession | null;
-  url: string;
-  method?: string;
-  success: (data: PromiseLike<string> | string) => void;
-  error: (error: Error) => void;
-  raw?: boolean;
-  requestBody?: any;
-  nonStale?: boolean;
-  tryGetServerInformation: () => ServerInformation | null;
-  getAntiForgeryTokenCallback: () => string | null;
-  onRequestCallback: (details: ClientRequestDetails) => void;
-  onResponseCallback: (details: ClientResponseDetails) => void;
-  onErrorResponseCallback: (details: ClientErrorResponseDetails) => void;
+    configuration: ClientConfiguration;
+    session: ClientSession | null;
+    url: string;
+    method?: string;
+    success: (data: PromiseLike<string> | string) => void;
+    error: (error: Error) => void;
+    raw?: boolean;
+    requestBody?: any;
+    nonStale?: boolean;
+    tryGetServerInformation: () => ServerInformation | null;
+    getAntiForgeryTokenCallback: () => string | null;
+    onRequestCallback: (details: ClientRequestDetails) => void;
+    onResponseCallback: (details: ClientResponseDetails) => void;
+    onErrorResponseCallback: (details: ClientErrorResponseDetails) => void;
 }
