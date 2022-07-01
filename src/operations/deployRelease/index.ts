1a6e7bf8d835c64184dba02aaaac2d1b24f3ef26
John Bristowe <john.bristowe@octopus.com>
2022-07-01 12:03:19 +1000

    chore: added index files for typescript type exports

---
+++ src/operations/deployRelease/index.ts
@@ -0,0 +1,4 @@
+export * from "./deploy-release";
+export * from "./deployment-base";
+export * from "./deployment-options";
+export * from "./execution-resource-waiter";
---
+++ src/operations/index.ts
@@ -0,0 +1,7 @@
+export * from "./could-not-find-error";
+export * from "./createRelease";
+export * from "./deployRelease";
+export * from "./promoteRelease";
+export * from "./pushBuildInformation";
+export * from "./pushPackage";
+export * from "./throw-if-undefined";
