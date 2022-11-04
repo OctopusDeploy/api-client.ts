d654864bd8ca6273c11309c5b27824ce27828c23
Shannon Lewis <slewis74@gmail.com>
2022-11-04 18:04:20 +1000

    chore: missed one of the methods that needed to add SpaceIdOrName to the command

--- src/operations/executions/deployRelease/deploy-release.ts
+++ src/operations/executions/deployRelease/deploy-release.ts
@@ -8,7 +8,12 @@ export async function deployReleaseUntenanted(
 ): Promise<CreateDeploymentUntenantedResponseV1> {
     console.log(`Deploying a release...`);
 
-    var response = await repository.client.do<CreateDeploymentUntenantedResponseV1>(`~/api/{spaceId}/deployments/create/untenanted/v1`, command);
+    // WARNING: server's API currently expects there to be a SpaceIdOrName value, which was intended to allow use of names/slugs, but doesn't
+    // work properly due to limitations in the middleware. For now, we'll just set it to the SpaceId
+    var response = await repository.client.do<CreateDeploymentUntenantedResponseV1>(`~/api/{spaceId}/deployments/create/untenanted/v1`, {
+        SpaceIdOrName: command.spaceId,
+        ...command,
+    });
 
     console.log(`Deployment created successfully.`);
 
