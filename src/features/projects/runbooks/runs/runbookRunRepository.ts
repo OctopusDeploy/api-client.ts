b69a91ac36a8af9bce339e19c8b54dda2549c01d
Shannon Lewis <slewis74@gmail.com>
2022-11-30 21:06:37 +1000

    chore: fixed path bug in runbookRunRepository

--- src/features/projects/runbooks/runs/runbookRunRepository.ts
+++ src/features/projects/runbooks/runs/runbookRunRepository.ts
@@ -52,7 +52,7 @@ export class RunbookRunRepository {
 
         // WARNING: server's API currently expects there to be a SpaceIdOrName value, which was intended to allow use of names/slugs, but doesn't
         // work properly due to limitations in the middleware. For now, we'll just set it to the SpaceId
-        const response = await this.client.doCreate<InternalCreateRunbookRunResponseV1>("runbook-runs/create/v1", {
+        const response = await this.client.doCreate<InternalCreateRunbookRunResponseV1>(`${spaceScopedRoutePrefix}/runbook-runs/create/v1`, {
             spaceIdOrName: command.spaceName,
             ...command,
         });
