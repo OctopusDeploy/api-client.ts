9cc21eff852bf26f396e06efbc0418e0907a4b25
Ben Pearce <ben.pearce@octopus.com>
2022-12-16 10:59:14 +1000

    fix it again

--- src/features/packages/packageRepository.ts
+++ src/features/packages/packageRepository.ts
@@ -60,15 +60,14 @@ export class PackageRepository {
 
         const completedTasks = await Promise.allSettled(tasks);
         for (const t of completedTasks) {
-            if (t.status != "rejected") {
-                rejectedTasks.push(t.reason);
+            if (t.status === "rejected") {
+                rejectedTasks.push(t.status);
             }
         }
 
         if (rejectedTasks.length > 0) {
             throw new Error(`${rejectedTasks}`);
         }
-
 
         this.client.info("Packages uploaded");
     }
