2ab89f4e9385de467f77ec3f33e2dfe354b44b5f
Ben Pearce <ben.pearce@octopus.com>
2023-02-27 14:19:54 +1000

    update request object

--- src/features/tenants/tenantRepository.ts
+++ src/features/tenants/tenantRepository.ts
@@ -24,7 +24,7 @@ export class TenantRepository extends SpaceScopedBasicRepository<Tenant, NewTena
     }
 
     getVariables(tenant: Tenant): Promise<TenantVariable> {
-        return this.client.request(`${spaceScopedRoutePrefix}/tenants/{id}/variables`, tenant);
+        return this.client.request(`${spaceScopedRoutePrefix}/tenants/{id}/variables`, { id: tenant.Id });
     }
 
     setVariables(tenant: Tenant, variables: any): Promise<TenantVariable> {
