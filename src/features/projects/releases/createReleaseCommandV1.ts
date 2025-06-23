22f1e830774c30b985c1179e81453a2c8e5c9c95
Lourens de Jager <165963988+lourens-octopus@users.noreply.github.com>
2025-06-24 10:49:53 +1200

    feat: Add Custom Fields to Create Release Command

--- src/features/projects/releases/createReleaseCommandV1.ts
+++ src/features/projects/releases/createReleaseCommandV1.ts
@@ -12,4 +12,5 @@ export interface CreateReleaseCommandV1 extends SpaceScopedOperation {
     IgnoreIfAlreadyExists?: boolean;
     IgnoreChannelRules?: boolean;
     PackagePrerelease?: string;
+    CustomFields?: Record<string, string>;
 }
