42cdc4b530dee0dd4655e97b2973c508602e89b2
wlthomson <will.tho@outlook.com>
2026-02-10 14:21:32 +1300

    Update GCS storage feed to extend RetryFeed

--- src/features/feeds/gcsStorageFeed.ts
+++ src/features/feeds/gcsStorageFeed.ts
@@ -2,13 +2,14 @@ import type { FeedType } from "./feedType";
 import type { SensitiveValue } from "../variables";
 import { SpaceScopedResource } from "../../spaceScopedResource";
 import { NamedResource } from "../../namedResource";
+import type { RetryFeed } from "./retryFeed";
 
 export interface GoogleOidcAuthentication {
     Audience?: string;
     SubjectKeys: string[];
 }
 
-export interface GcsStorageFeed extends SpaceScopedResource, NamedResource {
+export interface GcsStorageFeed extends SpaceScopedResource, NamedResource, RetryFeed {
     FeedType: FeedType.GcsStorage;
     Name: string;
     UseServiceAccountKey: boolean;
